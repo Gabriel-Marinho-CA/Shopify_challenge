1b351a5dabbaa2dc15a9c87196f48c48b8e325ed
Gabriel André <gabriel.andre@resultate.com.br>
2023-09-21 21:12:01 -0300

    collections in home page, passing variable throught snippet

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,14 @@ const gulp = require('gulp');
 // const themeKit = require('@shopify/themekit');
 const sass = require('gulp-sass')(require('sass'));
 var concat = require('gulp-concat');
+const clean = require('gulp-clean-css');
 
 gulp.task('sass', function () {
     return gulp.src('styles/custom-styles.scss')
         .pipe(sass())
+        .pipe(clean({
+            compatibility: 'ie11'
+        }))
         .pipe(concat('custom-styles.css.liquid'))
         .pipe(gulp.dest('assets/'))
 });
